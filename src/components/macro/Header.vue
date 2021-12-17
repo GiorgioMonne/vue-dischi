812cf83d971beadaa64d5b0dea5032e15650393a
Giorgio <1999giorgio.monne@mail.com>
2021-12-17 08:52:33 -1000

    progresso mattutino

---
+++ src/components/macro/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../../assets/img/spotify.png" alt="">
+        </div>
+    </header>    
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+
+    header{
+        background-color: rgb(66, 66, 90);
+    }
+
+    .logo{
+        height: 50px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+
+        img{
+            height: 30px;
+            margin: 0 15px;
+            cursor: pointer;
+        }
+    }
+
+</style>
---
+++ src/components/macro/Main.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+        <card/>  
+    </main>
+</template>
+
+<script>
+
+import card from '../commons/card.vue';
+export default {
+    name: 'Main',
+    components:{
+        card
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    main{
+        background-color: rgb(33, 39, 90);
+    }
+
+    .container{
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+</style>
